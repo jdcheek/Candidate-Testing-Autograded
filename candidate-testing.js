1e238129f1a244cc9307b0d8d1f1cce9b605101b
jdcheek <58921502+jdcheek@users.noreply.github.com>
2024-01-11 17:11:52 -0600

    updated readline sync variable name

--- candidate-testing.js
+++ candidate-testing.js
@@ -18,12 +18,12 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-	candidateName = readlineSync.question("What is your name?");
+	candidateName = input.question("What is your name?");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-	candidateAnswer = readlineSync.question(question);
+	candidateAnswer = input.question(question);
 }
 
 function gradeQuiz(candidateAnswers) {
